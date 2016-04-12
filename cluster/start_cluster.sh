3ab11876e1ccd59938882f667ed57d8c0aae05b7
root <root@yoda.daemonrob.com>
2016-04-12 23:17:45 +0800

    update

--- cluster/start_cluster.sh
+++ cluster/start_cluster.sh
@@ -2,13 +2,7 @@
 
 for i in `seq 1 3`; 
 do
-	redis-server /etc/redis/node$i/redis.conf
-	sleep 3
-done
-
-
-for i in `seq 1 3`; 
-do
-	redis-server /etc/redis/node$i/sentinel.conf --sentinel
+	set p = 27016 + $i
+	mongod -v --port $p --db /node$i --logpath /node$i/mongod.log --logRotate reopen --logappend
 	sleep 3
 done
