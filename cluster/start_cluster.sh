f4d37a7ae808153c075a36826e7b7ed3a181a9d0
root <root@yoda.daemonrob.com>
2016-04-12 23:27:01 +0800

    update

--- cluster/start_cluster.sh
+++ cluster/start_cluster.sh
@@ -3,6 +3,6 @@
 for i in `seq 1 3`; 
 do
 	set p = 27016 + $i
-	mongod -v --port $p --db /node$i --logpath /node$i/mongod.log --logRotate reopen --logappend
+	mongod --config /node$i/mongod.conf --logRotate reopen --logappend
 	sleep 3
 done
